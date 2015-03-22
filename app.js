96e02184b7187eb370b20134929fc96b85f1b1ab
Elizabeth Hale <lizzhale@mac.com>
2015-03-22 11:43:45 -0700

    Connected the app to the database and set up the add view

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var request = require('request');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+// var bodyParser = require('body-parser');
+// var methodOverride = require('method-override');
+var pg = require('pg');
+var models = require('./models/index.js');
 
 var app = express();
 var server = app.listen(3000);
 
-app.use(bodyParser.urlencoded({
-  extended:true
-}));
+// app.use(bodyParser.urlencoded({
+//   extended:true
+// }));
 
-app.use(methodOverride('_method'));
+// app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
 
