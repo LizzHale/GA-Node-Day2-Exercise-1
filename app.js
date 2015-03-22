ff4ab9b6a101cd89db8243c9e06caebc82efce19
Elizabeth Hale <lizzhale@mac.com>
2015-03-22 11:58:29 -0700

    Added user creation functionality

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var request = require('request');
-// var bodyParser = require('body-parser');
-// var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var pg = require('pg');
 var models = require('./models/index.js');
 
 var app = express();
 var server = app.listen(3000);
 
-// app.use(bodyParser.urlencoded({
-//   extended:true
-// }));
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
 
-// app.use(methodOverride('_method'));
+app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
 
@@ -26,4 +26,14 @@ app.get('/edit', function (req, res) {
 
 app.get('/add', function (req, res) {
   res.render('add');
+});
+
+app.post('/add', function (req, res) {
+  models.User.create({
+    first_name: req.body.firstname,
+    last_name: req.body.lastname,
+    age: req.body.age
+  }).success(function(data) {
+    res.redirect('/');
+  });
 });
